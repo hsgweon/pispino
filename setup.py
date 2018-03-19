41cfe67f90f90f2b55c09eefb2a553e7aa0c3266
Hyun Soon Gweon <soonio@gmail.com>
2018-03-19 10:00:08 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+
+__version__ = os.environ.get("VERSION", "0.1.0")
+
+setup(
+    name = "pispino",
+    version = __version__,
+    packages = ["pispino"],
+    scripts = ['bin/pispino_createreadpairslist', 'bin/pispino_seqprep'],
+    description = "PISPINO (PIpits SPIN-Off tools): Bioinformatics toolkits for processing NGS data.",
+    long_description = "An open source stand-alone suite of tools for processing NGS data. They were part of PIPITS, but subsequently became independent tools as they are extensively used for other pipelines developed by Gweon Lab.",
+    author = "Hyun Soon Gweon",
+    author_email = "h.s.gweon@reading.ac.uk",
+	url = "http://sourceforge.net/projects/pipits",
+	licence = "GPA"
+)
