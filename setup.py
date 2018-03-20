8f4ed889d217f8d3d14e96a39a9189b73b52968c
Hyun Soon Gweon <soonio@gmail.com>
2018-03-20 00:08:44 +0000

    first commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-__version__ = os.environ.get("VERSION", "0.1.0")
+__version__ = os.environ.get("VERSION", "1.0")
 
 setup(
     name = "pispino",
