fb6be9734b2a987ed684a8ea9096e7098d1ae4b6
Hyun Soon Gweon <soonio@gmail.com>
2018-03-22 01:00:21 +0000

    Minor bug fixes.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-__version__ = os.environ.get("VERSION", "1.0")
+__version__ = os.environ.get("VERSION", "1.1")
 
 setup(
     name = "pispino",
